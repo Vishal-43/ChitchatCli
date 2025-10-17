f5af99fca2200ebaf39b97882c8056cc0cddd1e8
Vishal-43 <vishalsodmise@gmail.com>
2025-10-17 16:41:09 +0530

    created server.py

--- server.py
+++ server.py
@@ -4,9 +4,29 @@ import time
 from datetime import datetime 
 import getpass
 
-clients = {}
-nicknames = set()
+while True:
+    try:
+        server_id = int(input("enter a server id (1024-65535): "))
+        if 1024 <= server_id <= 65535:
+            break
+        else:
+            print("please enter a valid port number between 1024 and 65535")
+    except ValueError:
+        print("please enter a valid integer port number")
 
+
+password = getpass.getpass("set a password for the server:")
+if not password:
+    print("password cannot be empty. exiting...")
+    exit()
+try:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0',server_id))
+    server.listen()
+    print(f"server started on port {server_id}")
+except Exception as e:
+    print(f"failed to start server: {e}")
+    exit()
 
 
     
